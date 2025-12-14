5efa13938499eb42f35d30c62b8c7a16e7966e06
Mitu-Rani-Sutradhar <mitusutradhar4965@gmail.com>
2025-12-14 20:29:49 +0600

    added interests for this crops section

--- src/pages/CropsDetails.jsx
+++ src/pages/CropsDetails.jsx
@@ -1,17 +1,27 @@
-import React, { use, useRef } from 'react';
+import React, { use, useEffect, useRef, useState } from 'react';
 import { useLoaderData } from 'react-router';
 import { AuthContext } from '../provider/AuthProvider';
 import Swal from 'sweetalert2';
 
-const CropsDetails = () => {
+    const CropsDetails = () => {
 
- 
+    const cropsDelails =useLoaderData();
+    console.log(cropsDelails);
+    const [interests, setInterests] = useState([])
 
     const interestModalRef = useRef(null);
     const { user } =use(AuthContext);
 
-    const cropsDelails =useLoaderData();
-    console.log(cropsDelails);
+    useEffect(() =>{
+        fetch(`http://localhost:3000/crops/interests/${cropsDelails._id}`)
+        .then(res => res.json())
+        .then(data =>{
+            console.log('interests for this product', data)
+           setInterests(data)
+        })
+    },[cropsDelails._id])
+
+    
 
     const handleInterestModalOpen = ()=>{
            interestModalRef.current.showModal();
@@ -65,6 +75,13 @@ const CropsDetails = () => {
       timer: 1500
           });
             
+        //   added the new interest to the state
+          newInterest._id = data.insertedId;
+          
+          const newInterests = [...interests, newInterest];
+          newInterests.sort((a,b)=> b.quantity - a.quantity)
+          setInterests(newInterests);
+
            }
        })
 
@@ -72,8 +89,9 @@ const CropsDetails = () => {
 
     return (
         <div>
-           <div className='left'>
-
+           <div className='flex justify-between w-11/12 mx-auto pt-10'>
+            <div className='left'>
+             right site
            </div>
            <div className='right'>
               <button onClick={handleInterestModalOpen} className='btn btn-primary'>Interest</button>
@@ -119,6 +137,66 @@ const CropsDetails = () => {
 </dialog>  
            
            </div>
+           </div>
+
+             <div>
+              <h2 className='text-3xl'>Interests for this crop:{interests.length}</h2>
+
+              <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th>
+            SL No.
+        </th>
+        <th>Buyer Name</th>
+        <th>Quantity</th>
+        <th>Message</th>
+        <th>Status</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* row 1 */}
+      
+      {
+        interests.map((interest, index) =>
+             <tr>
+        <th>
+          {index + 1}
+        </th>
+        <td>
+          <div className="flex items-center gap-3">
+            <div className="avatar">
+              
+            </div>
+            <div>
+              <div className="font-bold">{interest.userName}</div>
+             
+            </div>
+          </div>
+        </td>
+        <td>
+          {interest.quantity}
+        </td>
+        <td>{interest.message}</td>
+        <td>{interest.status}</td>
+        <td>{interest.Actions}</td>
+        <th>
+          <button className="btn btn-ghost btn-xs">details</button>
+        </th>
+      </tr>
+        )
+      }
+      
+      
+    </tbody>
+    
+  </table>
+</div>
+             </div>
+
         </div>
     );
 };
